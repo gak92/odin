4011f8da12b3fb3d9af6d96a6a15bb9fc8a85f30
gak92 <44802922+gak92@users.noreply.github.com>
2022-05-20 17:25:53 +0500

    update app.js improved game function to print the winner

--- javascript/rock paper scissors/app.js
+++ javascript/rock paper scissors/app.js
@@ -1,6 +1,8 @@
-// console.log("Rock Paper Scissors");
-
 const arr = ['Rock', 'Paper', 'scissors'];
+let playerSelection;
+let computerSelection;
+let playerScore = 0;
+let computerScore = 0;
 
 function computerPlay(){
     let len = arr.length;
@@ -17,23 +19,35 @@ function playRound(playerSelection, computerSelection){
     let cs = computerSelection.toLowerCase();
 
     if(cs == ps){
+        playerScore += 1;
         return "It's a tie: ";
     }
     else if(cs != ps) {
+        computerScore += 1;
         return `${cs} , 'beats', ${ps}`;
     }
 
 }
 
-const playerSelection = "rock";
-let computerSelection;
 
 function game(){
     for(let i=0; i<5; i++){
+        playerSelection = prompt("Enter your selection");
         computerSelection = computerPlay();
         console.log(playRound(playerSelection, computerSelection));
-
+        console.log("Player Score: ", playerScore);
+        console.log("Computer Score: ", computerScore);
     }
 }
 
 game();
+if(playerScore > computerScore){
+    console.log("You win the game.");
+    console.log("Player Score: ", playerScore);
+    console.log("Computer Score: ", computerScore);
+}
+else {
+    console.log("You lose the game");
+    console.log("Player Score: ", playerScore);
+    console.log("Computer Score: ", computerScore);
+}
